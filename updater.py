a6d83048a8249f7af4af7bcab1ea8948472d4aec
TheNooB <noobchannel2706@gmail.com>
2023-02-22 12:50:39 +0800

    Add code to detect current installed version of libreoffice

--- updater.py
+++ updater.py
@@ -1,4 +1,6 @@
 import requests
+import subprocess
+import re
 from bs4 import BeautifulSoup as bs
 from packaging import version
 
@@ -6,8 +8,21 @@ STABLE_CHECK_URL = "https://download.documentfoundation.org/libreoffice/stable/"
 
 class Updater:
     def __init__(self):
+        self.multiple_installation = False
+        self.installed = False
+        self.update_available = False
+        
         versions = self.contents_to_version(self.fetch_page(STABLE_CHECK_URL))
         self.latest_version = max(versions)
+        self.installed_version = self.check_current_version()
+        if len(self.installed_version) > 1:
+            self.multiple_installation = True
+            self.installed = True
+        elif len(self.installed_version) == 1:
+            self.installed = True
+            
+        if max(self.installed_version) < self.latest_version:
+            self.update_available = True
 
     @staticmethod
     def fetch_page(url):
@@ -26,7 +41,23 @@ class Updater:
     def contents_to_version(contents):
         return [version.parse(i[:-1]) for i in contents]
     
+    @staticmethod
+    def check_current_version():
+        pattern = re.compile("^libreoffice\d+\.\d+$")
+        
+        command = ["dpkg-query", "-W", "libreoffice*"]
+        execution = subprocess.run(command, capture_output=True)
+        output = execution.stdout.decode()
+        output = [i.split("\t") for i in output.split("\n")]
+        
+        installed = [i[1] for i in output if pattern.match(i[0])]
+        return [version.parse(i) for i in installed]
+    
     
 if __name__ == "__main__":
     updater = Updater()
     print(updater.latest_version)
+    print(updater.installed_version)
+    print(updater.multiple_installation)
+    print(updater.update_available)
+    print(updater.installed)
